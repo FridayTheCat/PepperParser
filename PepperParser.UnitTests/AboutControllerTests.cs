76fe2363cfb9f56afb74ab19d5133b405e64dd85
Dmitry Proc <demann99@mail.ru>
2022-12-09 18:51:36 +0300

    Add UnitTests for AboutController

---
+++ PepperParser.UnitTests/AboutControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using PepperParser.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperParser.UnitTests
+{
+    [TestFixture]
+    public class AboutControllerTests
+    {
+        private AboutController _controller;
+        private ViewResult? _viewResult;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new AboutController();
+            _viewResult = _controller.Index() as ViewResult;
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewResultNotNull()
+        {
+            Assert.IsNotNull(_viewResult);
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewBagTitle()
+        {
+            Assert.That(_viewResult?.ViewData["Title"], Is.EqualTo("О нас"));
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewBagText()
+        {
+            Assert.That(_viewResult?.ViewData["Text"], Is.EqualTo("Информация"));
+        }
+    }
+}
