531bd57452fa347f78d6d2cc1b6832d198d7b828
Dmitry Proc <demann99@mail.ru>
2022-12-09 19:02:04 +0300

    Add UnitTests for DonationController

---
+++ PepperParser.UnitTests/DonationControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using PepperParser.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperParser.UnitTests
+{
+    [TestFixture]
+    public class DonationControllerTests
+    {
+        private DonationController _controller;
+        private ViewResult? _viewResult;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new DonationController();
+            _viewResult = _controller.Index() as ViewResult;
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewResultNotNull()
+        {
+            Assert.IsNotNull(_viewResult);
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewBagTitle()
+        {
+            Assert.That(_viewResult?.ViewData["Title"], Is.EqualTo("Донат"));
+        }
+
+        [Test]
+        public void Test_Index_ReturnsViewBagText()
+        {
+            Assert.That(_viewResult?.ViewData["Text"], Is.EqualTo("Помощь сайту"));
+        }
+
+    }
+}
